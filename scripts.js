2a20c3ad3229c2dc8b0d3d8c661832efb3220248
Ali Haider <76524134+haiderminhas77@users.noreply.github.com>
2023-01-10 15:00:21 -0500

    adding js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var f = 0;
+var s = 0;
+    function buttonClick() {
+        f++;
+        document.getElementById('inc1').value = f;
+    }
+function buttonClick2() {
+        s++;
+        document.getElementById('inc2').value = s;
+    }
